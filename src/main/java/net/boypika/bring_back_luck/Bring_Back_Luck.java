44d7f2963efc52f1c1867e89714a10480879f6b3
BoyPika <52831958+BoyPika@users.noreply.github.com>
2022-10-03 23:48:11 -0400

    Create Bring_Back_Luck.java

---
+++ src/main/java/net/boypika/bring_back_luck/Bring_Back_Luck.java
@@ -0,0 +1,19 @@
+package net.boypika.bring_back_luck;
+
+import net.fabricmc.api.ModInitializer;
+import net.boypika.bring_back_luck.item.ModItems;
+import net.boypika.bring_back_luck.potion.ModPotions;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Bring_Back_Luck implements ModInitializer {
+	public static final String MOD_ID = "bring_back_luck";
+	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        ModItems.registerModItems();
+        ModPotions.registerPotions();
+
+    }
+}
