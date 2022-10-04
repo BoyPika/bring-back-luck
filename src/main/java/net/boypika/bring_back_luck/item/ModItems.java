cb26c6d0a1fce93a229895d1b7fb97714dbb24c5
BoyPika <52831958+BoyPika@users.noreply.github.com>
2022-10-03 23:45:56 -0400

    Create ModItems.java

---
+++ src/main/java/net/boypika/bring_back_luck/item/ModItems.java
@@ -0,0 +1,21 @@
+package net.boypika.bring_back_luck.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.boypika.bring_back_luck.Bring_Back_Luck;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+
+public class ModItems {
+    public static final Item LUCKY_CLOVER = registerItem("lucky_clover",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+    public static void registerModItems() {
+        Bring_Back_Luck.LOGGER.info("Working Idiot");
+    }
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(Bring_Back_Luck.MOD_ID, name), item);
+    }
+}
