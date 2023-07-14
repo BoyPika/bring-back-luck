0b3032357a2b1ea8eb710ba007ee88d22eb768b7
BoyPika <boypika85@gmail.com>
2023-07-14 11:31:52 -0400

    c

--- src/main/java/net/boypika/bring_back_luck/potion/ModPotions.java
+++ src/main/java/net/boypika/bring_back_luck/potion/ModPotions.java
@@ -1,6 +1,5 @@
 package net.boypika.bring_back_luck.potion;
 
-import net.boypika.bring_back_luck.Bring_Back_Luck;
 import net.boypika.bring_back_luck.mixin.BrewingRecipeRegistryMixin;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.item.Items;
@@ -15,30 +14,22 @@ import net.minecraft.entity.effect.StatusEffects;
 
 
 public class ModPotions {
-    public static Potion UNLUCK_POTION;
-    public static Potion registerPotion(String name) {
-        return Registry.register(Registries.POTION, new Identifier(Bring_Back_Luck.MOD_ID, name),
-                new Potion(new StatusEffectInstance(StatusEffects.UNLUCK, 3000, 0)));
-    }
-    public static Potion LONG_LUCK;
-    public static Potion registerPotion2(String name) {
-        return Registry.register(Registries.POTION, new Identifier(Bring_Back_Luck.MOD_ID, name),
-                new Potion(new StatusEffectInstance(StatusEffects.LUCK, 12000, 0)));
-    }
-    public static Potion LUCK2;
-    public static Potion registerPotion3(String name) {
-        return Registry.register(Registries.POTION, new Identifier(Bring_Back_Luck.MOD_ID, name),
-                new Potion(new StatusEffectInstance(StatusEffects.LUCK, 3000, 1)));
-    }
-    public static void registerPotions() {
-        UNLUCK_POTION = registerPotion("unlucky_potion");
-        LONG_LUCK = registerPotion2("long_luck_potion");
-        LUCK2 = registerPotion3("luck_2");
+    public static final Potion UNLUCK_POTION;
+    public static final Potion LONG_LUCK;
+    public static final Potion LUCK2;
 
-        registerPotionRecipes();
+    static {
+        UNLUCK_POTION = registerPotion("unlucky_potion", new Potion(new StatusEffectInstance(StatusEffects.UNLUCK, 3000, 0)));
+        LONG_LUCK = registerPotion("luck_2", new Potion(new StatusEffectInstance(StatusEffects.LUCK, 3000, 1)));
+        LUCK2 = registerPotion("long_luck_potion", new Potion(new StatusEffectInstance(StatusEffects.LUCK, 12000,0)));
     }
 
-    private static void registerPotionRecipes() {
+    public static Potion registerPotion(String name, Potion potion) {
+        return Registry.register(Registries.POTION, new Identifier("bring_back_luck", name), potion);
+    }
+
+
+    public static void registerPotionRecipes() {
         BrewingRecipeRegistryMixin.invokeRegisterPotionRecipe(Potions.AWKWARD, ModItems.LUCKY_CLOVER,
                 Potions.LUCK);
         BrewingRecipeRegistryMixin.invokeRegisterPotionRecipe(Potions.LUCK, Items.FERMENTED_SPIDER_EYE,
